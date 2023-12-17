2f433dd482a5a52ac02a70749319bf727801fdb5
Bunyamin Gokcen <bnymn.gkcn2@gmail.com>
2023-12-17 11:14:58 +0300

    First-Commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+let toDoList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < toDoList.length; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  toDoList[i].appendChild(span);
+}
+
+let close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    let div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+let list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+function newElement() {
+  let li = document.createElement("li");
+  li.classList.add('m-2')
+  let inputValue = document.getElementById("task").value; 
+  localStorage.setItem(`${inputValue}`, inputValue) // local storage'a input verisini eklemek için
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "" || inputValue.replace(/^\s+|\s+$/g, "").length == 0) {
+    $(".error").toast("show");
+  } else {
+    $(".success").toast("show");
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("task").value = "";
+
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
+
